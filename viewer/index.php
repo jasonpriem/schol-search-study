293005c4e044594fe80fbfa1021bd8c605aabda3
Jason Priem <jason@jasonpriem.com>
2011-11-24 00:33:13 -0500

    Search->render prints query, date, and SERP

--- viewer/index.php
+++ viewer/index.php
@@ -2,8 +2,21 @@
 include('./Log.php');
 include('./SearchLog.php');
 include('./ActivityLog.php');
+include('./Search.php');
+include('./SearchParser.php');
+date_default_timezone_set('America/New_York'); 
+$searchParser = new SearchParser(file_get_contents('./data/search.test'));
+$search = new Search($searchParser);
+$search->render();
 
-$log = new SearchLog("this is\nmystring\nright here");
-print_r($log->getLines());
 
 ?>
+<h2>Okay, what's this thing need to be able to do?</h2>
+<pre>
+* open the page log
+* make an array of clicks on each page, keyed by url
+* open the search text file
+* make an array of searches
+* parse each search
+* print the thing
+</pre>
